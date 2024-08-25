ff4f438915ca9c82d7ee5221c46a55a7216b591f
Devin McLaughlin <DRMcLaughlin@student.fullsail.edu>
2024-08-25 06:04:25 -0400

    Done with tool bar and methods for it

--- LifeOfGame/MainWindow.cpp
+++ LifeOfGame/MainWindow.cpp
@@ -2,15 +2,18 @@
 
 wxBEGIN_EVENT_TABLE(MainWindow, wxFrame)
 EVT_SIZE(MainWindow::WindowResize)
+EVT_MENU(TOOL_PLAY, MainWindow::OnPlay)
+EVT_MENU(TOOL_PAUSE, MainWindow::OnPause)
+EVT_MENU(TOOL_NEXT, MainWindow::OnNext)
+EVT_MENU(TOOL_TRASH, MainWindow::OnTrash)
 wxEND_EVENT_TABLE()
 
-MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "Game of Life", wxPoint(0, 0), wxSize(200, 200))		//frame constructor
+MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "Game of Life", wxPoint(0, 0), wxSize(200, 200))		
 {
-	statusBar = CreateStatusBar(2);
-	statusBar->SetStatusText("Generations: ", 0);
-	statusBar->SetStatusText("Living Cell: ", 1);
 	drawingPanel = new DrawingPanel(this, gameBoard);
 	InitializeGrid(gameBoard);
+	InitializeStatusBar();
+	InitializeToolBar();
 	this->Layout();
 }
 MainWindow::~MainWindow()
@@ -33,4 +36,44 @@ void MainWindow::InitializeGrid(std::vector<std::vector<bool>>& param)
 	}
 
 	drawingPanel->SetGridSize(gSize);
+}
+void MainWindow::InitializeStatusBar()
+{
+	statusBar = CreateStatusBar(2);
+	statusBar->SetStatusText("Generations: ", 0);
+	statusBar->SetStatusText("Living Cell: ", 1);
+}
+void MainWindow::InitializeToolBar()
+{
+	toolBar = CreateToolBar(4);
+
+	wxBitmap playIcon(play_xpm);
+	toolBar->AddTool(TOOL_PLAY, "Play", playIcon);
+
+	wxBitmap pauseIcon(pause_xpm);
+	toolBar->AddTool(TOOL_PAUSE, "Pause", pauseIcon);
+
+	wxBitmap nextIcon(next_xpm);
+	toolBar->AddTool(TOOL_NEXT, "Next", nextIcon);
+
+	wxBitmap trashIcon(trash_xpm);
+	toolBar->AddTool(TOOL_TRASH, "Trash", trashIcon);
+
+	toolBar->Realize();
+}
+void MainWindow::OnPlay(wxCommandEvent& event)
+{
+
+}
+void MainWindow::OnPause(wxCommandEvent& event)
+{
+
+}
+void MainWindow::OnNext(wxCommandEvent& event)
+{
+
+}
+void MainWindow::OnTrash(wxCommandEvent& event)
+{
+
 }
--- LifeOfGame/MainWindow.h
+++ LifeOfGame/MainWindow.h
@@ -1,8 +1,18 @@
 #pragma once
 #include "wx/wx.h"
 #include "DrawingPanel.h"
+#include "play.xpm"
+#include "pause.xpm"
+#include "next.xpm"
+#include "trash.xpm"
 
-
+enum IDs
+{
+    TOOL_PLAY = 1000,
+    TOOL_PAUSE = 1100,
+    TOOL_NEXT = 1200, 
+    TOOL_TRASH = 1300,
+};
 
 class MainWindow :      //Frame for holding application
     public wxFrame
@@ -12,16 +22,25 @@ private:
     size_t livingCells{};
 
 public:
-    wxStatusBar* statusBar;
+
     DrawingPanel* drawingPanel = nullptr;
     std::vector<std::vector<bool>> gameBoard;
     size_t gSize = 15;
+    wxStatusBar* statusBar;
+    wxToolBar* toolBar;
     
 
     MainWindow();
     ~MainWindow();
     void WindowResize(wxSizeEvent& event);
     void InitializeGrid(std::vector<std::vector<bool>>& param);
+    void InitializeStatusBar();
+    void InitializeToolBar();
+    void OnPlay(wxCommandEvent& event);
+    void OnPause(wxCommandEvent& event);
+    void OnNext(wxCommandEvent& event);
+    void OnTrash(wxCommandEvent& event);
     wxDECLARE_EVENT_TABLE();
 };
 
+ 
